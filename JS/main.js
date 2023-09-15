21cd821d3dfc7e4ae4cfea574c14ebffc9e60cf8
Oyindamola10 <samiatadegbenro@gmail.com>
2023-09-15 15:24:21 +0100

    Using console

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+/*Write a JS program that asks students for their names ,and  5 courses they are taking,
+ use this information to generate a student  card that will be outputed in the console*/
+
+ //Prompt the student for their names 
+ let studentName = prompt("Enter the name of student ");
+
+ // Prompt the student for  course title
+ let courseTitle1 = prompt("Enter the first course ");
+ let courseTitle2 = prompt("Enter the second course ");
+ let courseTitle3 = prompt("Enter the third course ");
+ let courseTitle4 = prompt("Enter the fourth course ");
+ let courseTitle5 = prompt("Enter the fifth course ");
+
+ //use console.log to generate output in the console
+
+ console.log("Name:" + "  " + + studentName)
+ console.log("first course: "  + "  " + courseTitle1)
+ console.log("second course:" + "  " + courseTitle2)
+ console.log("third course: "  + "  " + courseTitle3)
+ console.log("fourth course:" + "  " + courseTitle4)
+ console.log("fifth course: "  +  "  " +courseTitle5)
